1ecde4c4c294e18d6f36ea8952b85072d3b1afa4
sbehera309 <31716993+sbehera309@users.noreply.github.com>
2018-07-10 16:00:57 -0400

    Initial commit

---
+++ app/src/main/java/com/game/sbehe_000/blast/GameActivity.java
@@ -0,0 +1,73 @@
+package com.game.sbehe_000.blast;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Point;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Display;
+
+/**
+ * Created by sbehe_000 on 10/10/2017.
+ */
+
+public class GameActivity extends AppCompatActivity {
+    private GameView gameView;
+    private int level;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+
+        level = getIntent().getExtras().getInt("levelnum");
+
+        System.out.println(level);
+
+        gameView = new GameView(this, size.x, size.y, level);
+
+        setContentView(gameView);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        gameView.pause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        gameView.resume();
+    }
+    @Override
+    public void onBackPressed() {
+        gameView.pause();
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Are you sure you want to exit?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+
+                        Intent startMain = new Intent(Intent.ACTION_MAIN);
+                        startMain.addCategory(Intent.CATEGORY_HOME);
+                        startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(startMain);
+                        finish();
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                        gameView.resume();
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
+
+    }
+}
---
+++ app/src/main/java/com/game/sbehe_000/blast/LevelActivity.java
@@ -0,0 +1,69 @@
+package com.game.sbehe_000.blast;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageButton;
+
+/**
+ * Created by sbehe_000 on 10/10/2017.
+ */
+
+public class LevelActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ImageButton buttonEasy;
+    private ImageButton buttonMedium;
+    private ImageButton buttonHard;
+    Animation leftoright, righttoleft, downtoup;
+    public int level_mode; //Use level_mode as a reference to determine the level in Game_Activity
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_level_test);
+
+        leftoright = AnimationUtils.loadAnimation(this, R.anim.lefttoright);
+        righttoleft = AnimationUtils.loadAnimation(this,R.anim.righttoleft);
+        downtoup = AnimationUtils.loadAnimation(this, R.anim.downtoup);
+
+        //getting the button
+        buttonEasy = (ImageButton) findViewById(R.id.buttonEasy);
+        buttonEasy.setAnimation(leftoright);
+        buttonMedium = (ImageButton) findViewById(R.id.buttonMedium);
+        buttonMedium.setAnimation(downtoup);
+        buttonHard = (ImageButton) findViewById(R.id.buttonHard);
+        buttonHard.setAnimation(righttoleft);
+
+        //adding a click listener
+        buttonEasy.setOnClickListener(this);
+        buttonMedium.setOnClickListener(this);
+        buttonHard.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        //Set the level based on the button the user clicks and start the Game class.
+        if ( v == buttonEasy){
+            level_mode = 1;
+            Intent intent = new Intent(LevelActivity.this, GameActivity.class);
+            intent.putExtra("levelnum", 1);
+            startActivity(intent);
+        }
+        if( v == buttonMedium){
+            level_mode = 2;
+            Intent intent =  new Intent(LevelActivity.this, GameActivity.class);
+            intent.putExtra("levelnum", 2);
+            startActivity(intent);
+        }
+        if(v == buttonHard){
+            level_mode = 3;
+            Intent intent = new Intent(LevelActivity.this,GameActivity.class);
+            intent.putExtra("levelnum", 3);
+            startActivity(intent);
+        }
+    }
+}
